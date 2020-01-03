3121056a755368ec51a0847f9fb5972e21d2b7f7
lee <1159542665@qq.com>
2020-01-03 16:38:51 +0800

    修改 StaffVO

--- spuerhero-pojo/src/main/java/com/next/pojo/vo/StaffVO.java
+++ spuerhero-pojo/src/main/java/com/next/pojo/vo/StaffVO.java
@@ -1,83 +1,45 @@
 package com.next.pojo.vo;
 
 public class StaffVO {
-    /**
-     * 姓名
-     */
-    private String name;
-
-    /**
-     * 性别：
-     1： 男
-     0： 女
-     */
-    private Integer sex;
-
-    /**
-     * 人物照片
-     */
-    private String photo;
-    /**
-     * 演职人员id
-     */
     private String staffId;
-
-    /**
-     * 角色：
-     1：导演
-     2：演员
-     */
+    private String name;
+    private Integer sex;
+    private String photo;
     private Integer role;
-
-    /**
-     * 饰演人物
-     */
     private String actName;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
     public String getStaffId() {
         return staffId;
     }
-
     public void setStaffId(String staffId) {
         this.staffId = staffId;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getSex() {
+        return sex;
+    }
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
     public Integer getRole() {
         return role;
     }
-
     public void setRole(Integer role) {
         this.role = role;
     }
-
     public String getActName() {
         return actName;
     }
-
     public void setActName(String actName) {
         this.actName = actName;
     }
