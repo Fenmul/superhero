4cdf4bfd6811635c4eeaa8890cf5a8c060d78704
lee <1159542665@qq.com>
2020-01-03 17:22:41 +0800

    mapper.xml 扫描路径修改

--- superhero-mapper/src/main/java/com/next/mapper/StaffMapperCustom.java
+++ superhero-mapper/src/main/java/com/next/mapper/StaffMapperCustom.java
@@ -4,6 +4,7 @@ import com.next.mymapper.MyMapper;
 import com.next.pojo.Staff;
 import com.next.pojo.vo.StaffVO;
 import org.apache.ibatis.annotations.Param;
+//import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,7 +14,8 @@ public interface StaffMapperCustom extends MyMapper<Staff> {
      * 根据预告片的id和角色查询导演列表或者演员列表
      * 通过 param 注解实现映射
      */
-    public List<StaffVO> queryStaffs(
+//    @Select(" SELECT sta.id as staffId, sta.`name` as name, sta.sex as sex, sta.photo as photo, rel.role as role, rel.act_name as actName FROM staff_movie rel LEFT JOIN staff sta on rel.staff_id = sta.id WHERE rel.movie_id = #{trailerId} AND rel.role = #{role}")
+     List<StaffVO> queryStaffs(
             @Param(value = "trailerId") String trailerId,
             @Param(value = "role") Integer role);
 
