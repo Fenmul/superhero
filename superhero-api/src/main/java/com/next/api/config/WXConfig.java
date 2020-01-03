572f85edb2f3ebcf084b57b60600da9c26a8a0df
lee <1159542665@qq.com>
2020-01-03 21:46:53 +0800

    提交微信小程序相关参数配置

---
+++ superhero-api/src/main/java/com/next/api/config/WXConfig.java
@@ -0,0 +1,10 @@
+package com.next.api.config;
+
+/**
+ * 微信相关配置
+ */
+public class WXConfig {
+    public final static String appId = "wx19f8879a0ae7ee73";
+    public final static String secret = "";
+}
+
